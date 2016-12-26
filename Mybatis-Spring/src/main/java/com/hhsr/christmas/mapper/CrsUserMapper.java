a54de1b1319cee19f6db358283efd56d28627c73
hanhai <hanhai@qianmi.com>
2016-12-26 17:06:02 +0800

    delete

--- Mybatis-Spring/src/main/java/com/hhsr/christmas/mapper/CrsUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.hhsr.christmas.mapper;
-
-import com.hhsr.christmas.entity.CrsUser;
-import com.hhsr.christmas.util.MyMapper;
-
-public interface CrsUserMapper extends MyMapper<CrsUser> {
-}
--- Mybatis-Spring/src/main/java/com/hhsr/christmas/mapper/CrsWinnersMapper.java
+++
@@ -1,7 +0,0 @@
-package com.hhsr.christmas.mapper;
-
-import com.hhsr.christmas.entity.CrsWinners;
-import com.hhsr.christmas.util.MyMapper;
-
-public interface CrsWinnersMapper extends MyMapper<CrsWinners> {
-}
