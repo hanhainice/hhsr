33af4ed803233750ba4bf69b503912acbb8ac2db
hanhai <hanhai@qianmi.com>
2016-12-26 16:56:00 +0800

    first commit

---
+++ Mybatis-Spring/src/main/java/com/hhsr/christmas/mapper/CrsWinnersMapper.java
@@ -0,0 +1,7 @@
+package com.hhsr.christmas.mapper;
+
+import com.hhsr.christmas.entity.CrsWinners;
+import com.hhsr.christmas.util.MyMapper;
+
+public interface CrsWinnersMapper extends MyMapper<CrsWinners> {
+}
