bb8bf995ef803152129a6e1434d27f9099048b2b
hanhai <hanhai@qianmi.com>
2017-01-12 16:36:14 +0800

    日志

--- src/main/java/com/hhsr/christmas/controller/HomeController.java
+++ src/main/java/com/hhsr/christmas/controller/HomeController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by hanhai on 2017/1/6.
@@ -15,9 +14,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/home")
 public class HomeController {
 
-    @ResponseBody
     @RequestMapping(method = RequestMethod.GET)
     public String home(){
+        log.info("进入home+++++++++++");
         return "home";
     }
 
--- src/main/java/com/hhsr/christmas/controller/LoginController.java
+++ src/main/java/com/hhsr/christmas/controller/LoginController.java
@@ -1,7 +1,8 @@
 package com.hhsr.christmas.controller;
 
 import com.hhsr.christmas.entity.SecUser;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.UnauthorizedException;
@@ -18,9 +19,10 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * 登录
  */
-@Slf4j
 @Controller
 public class LoginController {
+
+    private static Logger log = LogManager.getLogger(LoginController.class);
 
 
     @RequestMapping(value="login",method=RequestMethod.GET)
